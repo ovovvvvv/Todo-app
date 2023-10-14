9cb9b2a3b10102fc699134d11deb9e1624431156
ovovvvvv <ovovvvvv@naver.com>
2023-10-14 22:43:01 +0900

    TodoList -> TodoListItem

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -5,7 +5,7 @@ const TodoList = ({ todos }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todos} key={todo.id} />
+        <TodoListItem todo={todo} key={todo.id} />
       ))}
     </div>
   );
