af0b9047be812b754a931d898ad066ce29e0d6ab
ovovvvvv <ovovvvvv@naver.com>
2023-10-16 16:10:08 +0900

    지우기 기능 구현하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -37,10 +37,17 @@ function App() {
     nextId.current += 1;
   });
 
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} />
     </TodoTemplate>
   );
 }
