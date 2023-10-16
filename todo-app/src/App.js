98c5640714906718561abc61481697958aea80d9
ovovvvvv <ovovvvvv@naver.com>
2023-10-16 17:44:52 +0900

    불변성의 중요성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -67,3 +67,18 @@ const App = () => {
 };
 
 export default App;
+
+// const todos = [
+//   { id: 1, checked: true },
+//   { id: 2, checked: true },
+// ];
+// const nextTodos = [...todos];
+
+// nextTodos[0].checked = false;
+// console.log(todos[0] === nextTodos[0]); // 아직까지는 똑같은 객체를 가리키고 있기 때문에 true
+
+// nextTodos[0] = {
+//   ...nextTodos[0],
+//   checked: false,
+// };
+// console.log(todos[0] === nextTodos[0]); // 새로운 객체를 할당해 주었기에 false
