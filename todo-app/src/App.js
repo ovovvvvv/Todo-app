6bbbabbfafac6739da74ba0d8a4ce7f0db382716
ovovvvvv <ovovvvvv@naver.com>
2023-10-16 16:58:26 +0900

    useState의 함수형 업데이트

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -28,27 +28,21 @@ const App = () => {
       text,
       checked: false,
     };
-    setTodos(todos.concat(todo));
+    setTodos((todos) => todos.concat(todo));
     nextId.current += 1;
   });
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
   return (
     <TodoTemplate>
